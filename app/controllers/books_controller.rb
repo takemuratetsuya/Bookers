616f06d045d0fc4b09f4d03ee3868647b4ba1c5e
takemuratetsuya <109710030+takemuratetsuya@users.noreply.github.com>
2022-08-17 13:59:12 +0000

    [Add]ルーティング　コントローラー　ビュー　追加　

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,25 @@ class BooksController < ApplicationController
   def create
     book = Book.new (book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path (book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find (params[:id])
   end
 
   def edit
+    @book = Book.find (params[:id])
+  end
+  
+  def update
+    book = book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
   
   private
@@ -23,3 +32,4 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 end
+ 
